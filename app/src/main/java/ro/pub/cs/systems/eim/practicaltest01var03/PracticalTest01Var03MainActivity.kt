356147ea44109f5630f6f14b8ccb6837820ed0d9
OvidiuBurea <ovimb2002@gmail.com>
2024-11-12 08:36:19 +0200

    task 1 done

--- app/src/main/java/ro/pub/cs/systems/eim/practicaltest01var03/PracticalTest01Var03MainActivity.kt
+++ app/src/main/java/ro/pub/cs/systems/eim/practicaltest01var03/PracticalTest01Var03MainActivity.kt
@@ -1,20 +1,45 @@
 package ro.pub.cs.systems.eim.practicaltest01var03
 
+import android.content.Intent
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class PracticalTest01Var03MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_practical_test01_var03_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        val number1 = findViewById<EditText>(R.id.number1)
+        val number2 = findViewById<EditText>(R.id.number2)
+        val result = findViewById<TextView>(R.id.result)
+        val buttonAdd = findViewById<Button>(R.id.button_add)
+        val buttonSubtract = findViewById<Button>(R.id.button_subtract)
+        val buttonNextActivity = findViewById<Button>(R.id.button_next_activity)
+
+        buttonAdd.setOnClickListener {
+            val num1 = number1.text.toString().toIntOrNull()
+            val num2 = number2.text.toString().toIntOrNull()
+            if (num1 != null && num2 != null) {
+                result.text = "${num1} + ${num2} = ${num1 + num2}"
+            } else {
+                Toast.makeText(this, "Please enter valid numbers", Toast.LENGTH_SHORT).show()
+            }
         }
+
+        buttonSubtract.setOnClickListener {
+            val num1 = number1.text.toString().toIntOrNull()
+            val num2 = number2.text.toString().toIntOrNull()
+            if (num1 != null && num2 != null) {
+                result.text = "${num1} - ${num2} = ${num1 - num2}"
+            } else {
+                Toast.makeText(this, "Please enter valid numbers", Toast.LENGTH_SHORT).show()
+            }
+        }
+
+
     }
 }
